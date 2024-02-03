56519d7e212be943fe4c733b3788d454ebd5661a
NguyenTHanhAn853202 <nguyenthanhanqt@gmail.com>
2024-02-03 23:43:51 +0700

    voicher card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Background from "../components/HomePage/background";
 import Introduction from "../components/HomePage/Introduction";
+import DisplayVoicher from "~/components/HomePage/DisplayVoicher";
 function Home() {
   return (
-    <div>
+    <div className="pb-5">
       <Background />
       <Introduction />
+      <DisplayVoicher />
     </div>
   );
 }

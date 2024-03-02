d570682796b1195505e217d99d9d3cf39962ee45
hoaian03 <dangtranhoaian8603@gmail.com>
2024-03-02 19:06:20 +0700

    scroll to top button

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,17 +1,46 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import classNames from "classnames/bind";
+import { MdOutlineKeyboardDoubleArrowUp } from "react-icons/md";
+
+import styles from "~/styles/ScrollToTop.module.scss";
+
+const cx = classNames.bind(styles);
 
 const ScrollToTop = () => {
     const { pathname } = useLocation();
-    useEffect(() => {
+    const [active, setActive] = useState(false);
+
+    const scrollFunction = () => {
         window.scrollTo({
             top: 0,
             left: 0,
             behavior: "smooth",
         });
+    };
+
+    useEffect(() => {
+        scrollFunction();
     }, [pathname]);
 
-    return null;
+    const toggleShowBtn = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 100) {
+            setActive(true);
+        } else {
+            setActive(false);
+        }
+    };
+
+    window.addEventListener("scroll", toggleShowBtn);
+
+    return (
+        <div className={cx("scroll-to-top", active ? "active" : "")}>
+            <button className={cx("btn-scroll")} onClick={scrollFunction}>
+                <MdOutlineKeyboardDoubleArrowUp className="fs-1" />
+            </button>
+        </div>
+    );
 };
 
 export default ScrollToTop;
